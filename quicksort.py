3994a4c14b8df2528d5460264dd4c5bd30b59120
CBen <895488201@qq.com>
2020-01-05 16:27:30 +0800

    init git test

---
+++ quicksort.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# 快排
+def quick_sort(numbers):
+    pass
+
+if __name__ == '__main__':
+    numbers = [3, 1, 9, 7, 2, 5]
+    print(quick_sort(numbers))
